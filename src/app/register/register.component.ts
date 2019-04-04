b0ce88665433037a3851d008d7500919ddc9f86c
Paul Taylor <paul@Pauls-MacBook-Pro-A1990.local>
2019-04-04 19:52:40 +1100

    remove unwanted code

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,7 +7,6 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
