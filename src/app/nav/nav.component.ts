a597b105aed1b14f541fd005e4f813d38503b01c
Paul Taylor <paul@Pauls-MacBook-Pro-A1990.local>
2019-04-05 22:27:52 +1100

    add routing & route guard

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,8 @@ import { AlertifyService } from '../services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+    private router: Router) {}
 
   ngOnInit() {}
 
@@ -23,6 +25,9 @@ export class NavComponent implements OnInit {
       error => {
         this.alertify.error(error);
         console.log(error);
+      }, () => {
+        console.log('login complete');
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -35,5 +40,6 @@ export class NavComponent implements OnInit {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
     console.log('logged out');
+    this.router.navigate(['home']);
   }
 }
